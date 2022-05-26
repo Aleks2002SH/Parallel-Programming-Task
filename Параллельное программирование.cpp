50d778122546116baecb0bbd15e304d30ea2b5a0
Aleksandra Sharonova <cessi-sharonova@mail.ru>
2022-05-26 22:56:39 +0300

    Final solution

--- Параллельное программирование.cpp
+++ Параллельное программирование.cpp
@@ -14,11 +14,14 @@
 #include <stdexcept>
 #include <string>
 #include <fstream>
-#include <filesystem>
+#include <cstdio>
+#include <sstream>
+#include <stdio.h>
 
 using namespace std;
-namespace fs = filesystem;
 
+//SafeQueue is a queue designed to work with "safely" with threads.
+//It is a template class so you can work with any type of data you want
 template<class T>
 class SafeQueue {
 	queue<T> q;
@@ -102,123 +105,150 @@ public:
 	}
 
 };
+
+
+//reading data one by one from two files and put the lesser one first
 void thread_work(SafeQueue<string>& q, int my_num)
 {
 	while (true)
 	{
 		string a, b;
-		fs::path cur_p = fs::current_path();
 		bool go = q.wait_pop(a, b);
-		if (!go)
+		if (!go) {
 			break;
+		}
 		q.set_me_working(my_num, true);
-		string new_file_name = a + b;
-		ofstream out(new_file_name, ios::binary);
-		ifstream in1(a, ios::binary);
-		ifstream in2(b, ios::binary);
-		int num1, num2;
-		in1.read((char*)&num1, sizeof(int));
-		in2.read((char*)&num2, sizeof(int));
-		while (in1.peek() != EOF && in2.peek() != EOF) {
-			if (num1 < num2) {
-				out.write((char*)&num1, sizeof(num1));
-				in1.read((char*)&num1, sizeof(int));
+		FILE* file1, * file2, * result;
+		file1 = fopen(a.c_str(), "rb+");
+		file2 = fopen(b.c_str(), "rb+");
+		int buf1 = 0, buf2 = 0, c1, c2;
+		string filename = to_string(std::hash<std::thread::id>{}(std::this_thread::get_id()));
+		result = fopen(filename.c_str(), "wb+");
+		while (true)
+		{
+			c1 = fread(&buf1, sizeof(int), 1, file1);
+			c2 = fread(&buf2, sizeof(int), 1, file2);
+			if (c1 * c2 > 0)
+			{
+				if (buf1 >= buf2)
+				{
+					fseek(file2, -int(sizeof(int)), SEEK_CUR);
+					fwrite(&buf1, sizeof(int), 1, result);
+				}
+				else
+				{
+					fseek(file1, -int(sizeof(int)), SEEK_CUR);
+					fwrite(&buf2, sizeof(int), 1, result);
+				}
 			}
-			else {
-				out.write((char*)&num2, sizeof(num2));
-				in1.read((char*)&num2, sizeof(int));
+			else
+			{
+				if (c1 == 0)
+				{
+					fwrite(&buf2, sizeof(int), 1, result);
+					int abs = 1;
+					int* buf_abs = new int[100000];
+					while (abs != 0)
+					{
+						abs = fread(buf_abs, sizeof(int), 100000, file2);
+						fwrite(buf_abs, sizeof(int), abs, result);
+					}
+					delete[] buf_abs;
+				}
+				else
+				{
+					fwrite(&buf1, sizeof(int), 1, result);
+					int abs = 1;
+					int* buf_abs = new int[10000];
+					while (abs != 0)
+					{
+						abs = fread(buf_abs, sizeof(int), 10000, file1);
+						fwrite(buf_abs, sizeof(int), abs, result);
+					}
+					delete[] buf_abs;
+				}
+				break;
 			}
 		}
-		if (in1.peek() == EOF) {
-			while (in2.peek() != EOF) {
-				out.write((char*)&num2, sizeof(num2));
-				in1.read((char*)&num2, sizeof(int));
-			}
-		}
-		else {
-			out.write((char*)&num1, sizeof(num1));
-			in1.read((char*)&num1, sizeof(int));
-		}
-		in1.close();
-		in2.close();
-		remove(cur_p / a);
-		remove(cur_p / b);
-		out.close();
-		this_thread::sleep_for(chrono::milliseconds(100));
-		q.push(new_file_name);
+		fclose(result);
+		fclose(file1);
+		fclose(file2);
+		q.push(filename);
+		remove(a.c_str());
+		remove(b.c_str());
 		q.set_me_working(my_num, false);
 	}
 	q.set_me_working(my_num, false);
 }
 
-
-string merged_sorted_file(SafeQueue<string>& q, int req_num_treads)
+// function for initialising work of merging all created files
+void merged_sorted_file(SafeQueue<string>& q, int req_num_treads)
 {
-	int max_threads = thread::hardware_concurrency();
+	cout << "beginning merging" << endl;
+	queue<string> default_queue;
+	int max_threads = thread::hardware_concurrency(); 
 	int num_threads = min(max_threads, req_num_treads);
 	vector<thread> threads(num_threads - 1);
-
 	try
 	{
 		int tres, tstart, tend;
 		auto start = std::chrono::steady_clock::now();
 
 		for (int i = 0; i < num_threads - 1; i++)
-			threads[i] = thread(thread_work, ref(q), i);
-		thread_work(ref(q), num_threads - 1);
-
+		{
+			threads[i] = thread(thread_work, ref(q),  i);;
+		}
+		thread_work(ref(q), num_threads - 1); 
 		for_each(threads.begin(), threads.end(), mem_fn(&thread::join));
-
 		auto end = std::chrono::steady_clock::now();
 		auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
-
-		cout << "Done by " << num_threads << " thread(s) in " << elapsed.count() << " milisecs." << endl;
-
-		return q.just_pop();
+		cout << "Process 2 (merging)" << endl;
+		cout << "Done by " << num_threads << " thread(s) in " << elapsed.count() << " millisecs." << endl;
 
 	}
+
 	catch (...)
 	{
+		cout << "that's a flop" << endl;
 		for_each(threads.begin(), threads.end(), mem_fn(&thread::join));
 		throw;
 	}
 
 }
 
+//function to read given count of ints from file
 int read_from(ifstream& in, int N, vector<int>& V, int pos, SafeQueue<string>& q) {
 	int iter = 0;
-	cout << "Id потока " << this_thread::get_id() << endl;
-	try {
-		string filename = to_string(pos);
-		ofstream out(filename, ios::binary);
-		q.push(filename);
-		while (in.peek() != EOF && iter < N) {
-			int i;
-			in.read((char*)&i, sizeof(int));
-			iter++;
-			V.push_back(i);
-		}
-		sort(V.begin(), V.end());
-		for (int i = 0; i < V.size(); i++) {
-			out.write((char*)&V[i], sizeof(V[i]));
-		}
-		out.close();
+	cout << "Thread " << this_thread::get_id() << endl;
+	string filename = to_string(pos);
+	ofstream out(filename, ios::binary);
+	q.push(filename);
+	while (in.peek() != EOF && iter < N) {
+		int i;
+		in.read((char*)&i, sizeof(int));
+		iter++;
+		V.push_back(i);
 	}
-	catch (...) {
-		cout << "Oops" << endl;
+	sort(V.begin(), V.end(), std::greater<int>());
+	for (int i = 0; i < V.size(); i++) {
+		out.write((char*)&V[i], sizeof(V[i]));
 	}
+	out.close();
 	return iter;
 }
-void f(mutex& m, ifstream& in, int pos,int &count, SafeQueue<string>& q) {
+
+// function to start reading process from file
+void f(mutex& m, ifstream& in, int pos, int& count, SafeQueue<string>& q) {
 	lock_guard<mutex> guard(m);
 	vector<int> V;
 	int cnt = read_from(in, count, V, pos, q);
-	cout << "Количество прочитанных файлов " << cnt << endl;
+	/*cout << "Количество прочитанных файлов " << cnt << endl;
 	for (int i = 0; i < V.size(); i++) {
 		cout << V[i] << " " << endl;
-	}
+	}*/
 }
 
+// function to count all ints in file and give each threads required number of ints to read
 long int count_of_int(ifstream& in) {
 	in.seekg(0, ios::end);
 	long int n = in.tellg();
@@ -226,36 +256,47 @@ long int count_of_int(ifstream& in) {
 	return n / sizeof(int);
 }
 
+//assign reading process
+void process_1(ifstream& in, SafeQueue<string>& q) {
+	mutex m;
+	int max_threads = thread::hardware_concurrency();
+	vector<thread> threads(max_threads - 1);
+	int tres, tstart, tend;
+	auto start = std::chrono::steady_clock::now();
+	int full_cnt = (int)count_of_int(in);
+	int cnt =  full_cnt / (max_threads - 1);
+	for (int i = 0; i < max_threads - 2; i++)
+	{
+		threads[i] = thread(f,ref(m), ref(in), i * cnt, ref(cnt), ref(q));
+	}
+	int rest_cnt = full_cnt - cnt * (max_threads - 2);
+	threads[max_threads - 2] = thread(f, ref(m), ref(in), (max_threads - 1) * cnt, ref(rest_cnt), ref(q));
+	f(ref(m), ref(in), (max_threads - 1) * cnt, ref(rest_cnt), ref(q));
+	for_each(threads.begin(), threads.end(), mem_fn(&thread::join));
+	auto end = std::chrono::steady_clock::now();
+	auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+
+	cout << "Process 1 (dividing)" << endl;
+	cout << "Done by " << max_threads << " thread(s) in " << elapsed.count() << " millisecs." << endl;
+}
+
+
+
 int main()
 {
-	//Creating files to test work of the programm
-
 	ofstream out("test1.dat", ios::binary);
 	int N = 100;
 	for (int i = 1; i < N + 1; i++) {
 		out.write((char*)&i, sizeof(i));
 	}
 	out.close();
-
-	//Begin of the programm
-
 	ifstream in("test1.dat", ios::binary);
-	mutex m;
-	queue<string> default_queue;
+	queue<string> def_queue;
 	int max_threads = thread::hardware_concurrency();
-	cout << "Максимальное количество потоков " << max_threads << endl;
-	vector<thread> threads(max_threads);
-	SafeQueue<string> q(default_queue, max_threads);
-	long int intsinfile = count_of_int(in);
-	int cnt = (int)intsinfile / max_threads;
-	for (int i = 0; i < max_threads-1; i++)
-		threads[i] = thread(f, ref(m), ref(in), i * cnt,ref(cnt), ref(q));
-	cnt = (int)intsinfile - (max_threads - 1) * cnt;
-	threads[max_threads-1] = thread(f, ref(m), ref(in),  (max_threads-1)* cnt, ref(cnt), ref(q));
-	for_each(threads.begin(), threads.end(), mem_fn(&thread::join));
+	SafeQueue<string> q(def_queue,max_threads);
+	process_1(in, q);
 	in.close();
-	string file_name = merged_sorted_file(q, max_threads);
-	cout << file_name << endl;
+	merged_sorted_file(q, 4);
 	return 0;
 }
 
